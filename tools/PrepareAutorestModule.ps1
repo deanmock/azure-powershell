eac38f2923935a6626ecaf1dce462f1f36d0320a
wyunchi-ms <yunwang@microsoft.com>
2021-06-29 17:45:49 +0800

    Sync azure pipeline configuration with main branch

--- tools/PrepareAutorestModule.ps1
+++ tools/PrepareAutorestModule.ps1
@@ -66,8 +66,8 @@ git remote add -f origin https://github.com/Azure/azure-powershell.git
 git config core.sparseCheckout true
 Add-Content -Path .git/info/sparse-checkout -Value "src/Accounts/"
 git pull origin main
-Move-Item -Path "$TmpFolder\src\Accounts" -Destination "$TmpFolder\Accounts"
-Copy-Item "$TmpFolder\Accounts" "$PSScriptRoot\..\src" -Recurse -Force
+Move-Item -Path "$TmpFolder\src\Accounts" -Destination "$PSScriptRoot\..\src"
+Remove-Item -Path "$TmpFolder\src" -Recurse -Force
 Install-Module Az.Accounts -Repository PSGallery -Force
 Import-Module Az.Accounts
 Copy-Item "$PSScriptRoot\..\src\*.props" $TmpFolder
