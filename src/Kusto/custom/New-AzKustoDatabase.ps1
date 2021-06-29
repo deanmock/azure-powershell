da7171791dd923f3010cc8133806f931e064288f
wyunchi-ms <yunwang@microsoft.com>
2021-06-29 13:55:13 +0800

    Sync azure pipeline configuration with main branch

--- src/Kusto/custom/New-AzKustoDatabase.ps1
+++ src/Kusto/custom/New-AzKustoDatabase.ps1
@@ -1,3 +1,30 @@
+
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.Synopsis
+Creates or updates a database.
+.Description
+Creates or updates a database.
+.Example
+PS C:\> New-AzKustoDatabase -ResourceGroupName testrg -ClusterName testnewkustocluster -Name mykustodatabase -Kind ReadWrite -Location 'East US'
+
+Kind      Location Name                                Type
+----      -------- ----                                ----
+ReadWrite East US  testnewkustocluster/mykustodatabase Microsoft.Kusto/Clusters/Databases
+#>
 function New-AzKustoDatabase {
     [OutputType([Microsoft.Azure.PowerShell.Cmdlets.Kusto.Models.Api202101.IDatabase])]
     [CmdletBinding(PositionalBinding = $false, SupportsShouldProcess, ConfirmImpact = 'Medium')]
