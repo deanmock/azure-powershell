20e8978a6207761c9cc7b76173125f2fc90479e1
wyunchi-ms <yunwang@microsoft.com>
2021-06-29 11:09:00 +0800

    Sync azure pipeline configuration with main branch

--- src/HanaOnAzure/custom/New-AzSapMonitorProviderInstance.ps1
+++ src/HanaOnAzure/custom/New-AzSapMonitorProviderInstance.ps1
@@ -1,28 +1,3 @@
-
-# ----------------------------------------------------------------------------------
-#
-# Copyright Microsoft Corporation
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ----------------------------------------------------------------------------------
-
-<#
-.Synopsis
-Creates a provider instance for the specified subscription, resource group, SapMonitor name, and resource name.
-.Description
-Creates a provider instance for the specified subscription, resource group, SapMonitor name, and resource name.
-.Outputs
-Microsoft.Azure.PowerShell.Cmdlets.HanaOnAzure.Models.Api20200207Preview.IProviderInstance
-.Link
-https://docs.microsoft.com/powershell/module/az.hana/new-azsapproviderinstance
-#>
 function New-AzSapMonitorProviderInstance {
     [OutputType([Microsoft.Azure.PowerShell.Cmdlets.HanaOnAzure.Models.Api20200207Preview.IProviderInstance])]
     [CmdletBinding(DefaultParameterSetName = 'ByString', PositionalBinding = $false, SupportsShouldProcess, ConfirmImpact = 'Medium')]
